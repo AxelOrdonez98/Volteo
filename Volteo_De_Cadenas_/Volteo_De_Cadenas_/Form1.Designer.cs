5e8dd0d7dd22a97825da5f1b55ce445a3485af1a
Axel Ordoñez <Oscar_Ortiz9012@jabil.com>
2021-10-02 21:08:54 -0500

    actualizacion se copiado

--- Volteo_De_Cadenas_/Volteo_De_Cadenas_/Form1.Designer.cs
+++ Volteo_De_Cadenas_/Volteo_De_Cadenas_/Form1.Designer.cs
@@ -32,80 +32,108 @@ namespace Volteo_De_Cadenas_
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txt_Cadena_A_Voltear = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lb_Respuesta = new System.Windows.Forms.Label();
             this.btn_Voltear = new System.Windows.Forms.Button();
-            this.ltsb_Respuesta = new System.Windows.Forms.ListBox();
             this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtb_recipiente = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_Pass_Fail = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_Cadena_A_Voltear
             // 
-            this.txt_Cadena_A_Voltear.Location = new System.Drawing.Point(60, 53);
+            this.txt_Cadena_A_Voltear.Location = new System.Drawing.Point(12, 100);
             this.txt_Cadena_A_Voltear.Name = "txt_Cadena_A_Voltear";
-            this.txt_Cadena_A_Voltear.Size = new System.Drawing.Size(174, 20);
+            this.txt_Cadena_A_Voltear.Size = new System.Drawing.Size(290, 20);
             this.txt_Cadena_A_Voltear.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(263, 39);
+            this.label1.Size = new System.Drawing.Size(298, 60);
             this.label1.TabIndex = 1;
-            this.label1.Text = "1.- Copiar y pegar la cadena que se requieren voltear. \r\n2.- La cadena debe estar" +
-    " separada con: (,)(.)(-)(_)( )\r\n2.- Dar en el boton \"Voltear\".";
-            // 
-            // lb_Respuesta
-            // 
-            this.lb_Respuesta.AutoSize = true;
-            this.lb_Respuesta.Location = new System.Drawing.Point(9, 127);
-            this.lb_Respuesta.Name = "lb_Respuesta";
-            this.lb_Respuesta.Size = new System.Drawing.Size(0, 13);
-            this.lb_Respuesta.TabIndex = 2;
+            this.label1.Text = "1.- Copiar y pegar la cadena que se requieren voltear.\r\n2.- Introducir la ruta pa" +
+    "ra guardar.\r\n3.- La cadena debe estar separada con: (,)(-)(_)( )\r\n4.- Dar en el " +
+    "boton \"Voltear\".";
             // 
             // btn_Voltear
             // 
-            this.btn_Voltear.Location = new System.Drawing.Point(60, 79);
+            this.btn_Voltear.Location = new System.Drawing.Point(12, 167);
             this.btn_Voltear.Name = "btn_Voltear";
-            this.btn_Voltear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Voltear.Size = new System.Drawing.Size(124, 23);
             this.btn_Voltear.TabIndex = 3;
             this.btn_Voltear.Text = "Voltear";
             this.btn_Voltear.UseVisualStyleBackColor = true;
             this.btn_Voltear.Click += new System.EventHandler(this.btn_Voltear_Click);
             // 
-            // ltsb_Respuesta
-            // 
-            this.ltsb_Respuesta.FormattingEnabled = true;
-            this.ltsb_Respuesta.Location = new System.Drawing.Point(15, 108);
-            this.ltsb_Respuesta.Name = "ltsb_Respuesta";
-            this.ltsb_Respuesta.Size = new System.Drawing.Size(271, 329);
-            this.ltsb_Respuesta.TabIndex = 4;
-            // 
             // btn_Limpiar
             // 
-            this.btn_Limpiar.Location = new System.Drawing.Point(159, 79);
+            this.btn_Limpiar.Location = new System.Drawing.Point(178, 167);
             this.btn_Limpiar.Name = "btn_Limpiar";
-            this.btn_Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Limpiar.Size = new System.Drawing.Size(124, 23);
             this.btn_Limpiar.TabIndex = 5;
             this.btn_Limpiar.Text = "Limpiar";
             this.btn_Limpiar.UseVisualStyleBackColor = true;
             this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cadena:";
+            // 
+            // txtb_recipiente
+            // 
+            this.txtb_recipiente.Location = new System.Drawing.Point(12, 141);
+            this.txtb_recipiente.Name = "txtb_recipiente";
+            this.txtb_recipiente.Size = new System.Drawing.Size(290, 20);
+            this.txtb_recipiente.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ruta:";
+            // 
+            // lb_Pass_Fail
+            // 
+            this.lb_Pass_Fail.AutoSize = true;
+            this.lb_Pass_Fail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Pass_Fail.Location = new System.Drawing.Point(121, 214);
+            this.lb_Pass_Fail.Name = "lb_Pass_Fail";
+            this.lb_Pass_Fail.Size = new System.Drawing.Size(0, 15);
+            this.lb_Pass_Fail.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(302, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(314, 260);
+            this.Controls.Add(this.lb_Pass_Fail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtb_recipiente);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btn_Limpiar);
-            this.Controls.Add(this.ltsb_Respuesta);
             this.Controls.Add(this.btn_Voltear);
-            this.Controls.Add(this.lb_Respuesta);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_Cadena_A_Voltear);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "AE - Voltear valores";
+            this.Text = "AE - Flip";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -116,10 +144,12 @@ namespace Volteo_De_Cadenas_
 
         private System.Windows.Forms.TextBox txt_Cadena_A_Voltear;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lb_Respuesta;
         private System.Windows.Forms.Button btn_Voltear;
-        private System.Windows.Forms.ListBox ltsb_Respuesta;
         private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtb_recipiente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_Pass_Fail;
     }
 }
 
--- Volteo_De_Cadenas_/Volteo_De_Cadenas_/Form1.cs
+++ Volteo_De_Cadenas_/Volteo_De_Cadenas_/Form1.cs
@@ -14,42 +14,42 @@ namespace Volteo_De_Cadenas_
         {
             if (txt_Cadena_A_Voltear.Text != "")
             {
-                lb_Respuesta.Text = "";
                 string str_test = txt_Cadena_A_Voltear.Text;
+                str_test += ",";
                 string str_Acum = "";
                 int i_total_de_separado = 0;
-                ltsb_Respuesta.Items.Clear();
                 List<string> L_mostrar = new List<string>();
                 for (int i = 0; i < str_test.Length; ++i)
                 {
                     str_Acum += str_test[i];
-                    if (str_Acum.Contains(" ") || str_Acum.Contains(",") || str_Acum.Contains(".") || str_Acum.Contains("-") || str_Acum.Contains("_"))
+                    if (str_Acum.Contains(" ") || str_Acum.Contains(",") || str_Acum.Contains("-") || str_Acum.Contains("_"))
                     {
+                        str_Acum = str_Acum.TrimEnd(str_test[i]);
                         L_mostrar.Add(str_Acum);
                         str_Acum = "";
                     }
                 }
                 i_total_de_separado = L_mostrar.Count;
                 L_mostrar.Reverse();
+                string str_reverse = "";
                 for (int i = 0; i < i_total_de_separado; i++)
                 {
-                    ltsb_Respuesta.Items.Add(L_mostrar[i]);
+                    str_reverse += L_mostrar[i] + "\n";
                 }
-            }
-            else
-            {
-                MessageBox.Show("Verifique que campo de texto no este vacio", "ERROR");
+                txtb_recipiente.Text = str_reverse;
+                MessageBox.Show(str_reverse, "");
             }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
         private void btn_Limpiar_Click(object sender, EventArgs e)
         {
-            ltsb_Respuesta.Items.Clear();
+            //1 2 3 4 5 6 7 8 9
+            //ltsb_Respuesta.Items.Clear();
             txt_Cadena_A_Voltear.Clear();
+            txtb_recipiente.Text = "";
         }
     }
 }
